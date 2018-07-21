79b4d59bf5f1c458c893d4b122451c7adf5a659e
Karl Amoguis <ktamoguis@gmail.com>
2018-07-21 03:58:36 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,11 @@ class UsersController < ApplicationController
     @user = User.create(name: params[:user][:name], password: params[:user][:password])
     #binding.pry
     if @user.authenticate(params[:user][:password_confirmation])
+<<<<<<< HEAD
       #binding.pry
+=======
+      binding.pry
+>>>>>>> 4a9a8c978083d73de35b5d5779b547879e66649d
       session[:user_id] = @user.id
       redirect_to users_index_path
     else
